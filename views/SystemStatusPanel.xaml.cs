774d20866739da34e9940c8bc996dd7257677748
hek-git <ghddmsrl1@naver.com>
2026-04-28 13:34:13 +0900

    feat: 시스템 상태 관련 코드 추가 - 시스템 상태 관련 View 패널 추가

---
+++ views/SystemStatusPanel.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace AquaVectorUI.views
+{
+    public partial class SystemStatusPanel : UserControl
+    {
+        public SystemStatusPanel() => InitializeComponent();
+    }
+}
